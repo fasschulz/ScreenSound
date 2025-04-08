ca0bf0e1ab36fe6e60efc1b489b16147c3b5843e
Fernando Schulz <fasschulz@gmail.com>
2025-04-08 16:58:55 -0300

    Testando DELETE Musica

---
+++ ScreenSound.Integration.Test/Scenarios/MusicaTests/Musica_DELETE.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScreenSound.Integration.Test.FakeData;
+using ScreenSound.Integration.Test.Fixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Integration.Test.Scenarios.MusicaTests;
+
+[Collection(nameof(ScreeSoundWebApplicationFactoryCollection))]
+public class Musica_DELETE : IDisposable
+{
+    private readonly ScreenSoundWebApplicationFactory _app;
+    private readonly MusicaFakeData _musicaFakeData;
+
+    public Musica_DELETE(ScreenSoundWebApplicationFactory app)
+    {
+        _app = app;
+        _musicaFakeData = new MusicaFakeData(app);
+        _musicaFakeData.CriarDadosFake(5);
+    }
+
+    public void Dispose()
+    {
+        _musicaFakeData.LimparDadosDoBanco();
+    }
+
+    [Fact]
+    public async Task Deleta_Musica_Por_Id()
+    {
+        var musica = await _app.Context.Musicas.FirstOrDefaultAsync();
+        using var client = _app.CreateClient();
+
+        var response = await client.DeleteAsync("/Musicas/" + musica.Id);
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+}
