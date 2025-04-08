ccfd17e96e7912586c191cc192794de4df566a12
Fernando Schulz <fasschulz@gmail.com>
2025-04-08 10:49:41 -0300

    Teste endpoint Artista Delete

---
+++ ScreenSound.Integration.Test/Artista_DELETE.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScreenSound.Integration.Test.FakeData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Integration.Test;
+
+[Collection(nameof(ScreeSoundWebApplicationFactoryCollection))]
+public class Artista_DELETE : IDisposable
+{
+    private readonly ScreenSoundWebApplicationFactory _app;
+    private readonly ArtistaFakeData _artistaFakeData;
+
+    public Artista_DELETE(ScreenSoundWebApplicationFactory app)
+    {
+        _app = app;
+        _artistaFakeData = new ArtistaFakeData(app);
+        _artistaFakeData.CriarDadosFake(5);
+    }
+
+    public void Dispose()
+    {
+        _artistaFakeData.LimparDadosDoBanco();
+    }
+
+    [Fact]
+    public async Task Deleta_Artista_Por_Id()
+    {        
+        var artistaExistente = await _app.Context.Artistas.FirstOrDefaultAsync();
+        using var client = _app.CreateClient();
+
+        var response = await client.DeleteAsync("/Artistas/" + artistaExistente.Id);
+
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+}
